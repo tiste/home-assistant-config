d96f1e480d1bbb654b96e6183853f55d0ec78973
Baptiste Lecocq <hello@tiste.io>
2018-04-16 19:02:02 +0200

    feat: update linky component

---
+++ custom_components/sensor/README.md
@@ -0,0 +1,11 @@
+# Linky sensor
+
+## Usage
+
+`configuration.yaml`:
+```
+sensor:
+  - platform: linky
+    username: !secret linky_username
+    password: !secret linky_password
+```
--- custom_components/sensor/linky.py
+++ custom_components/sensor/linky.py
@@ -1,51 +1,51 @@
 """
 Support for Linky.
 For more details about this component, please refer to the documentation at
-http://github.com/tiste/roost/custom_components/linky.py/
+https://home-assistant.io/components/sensor.linky/
 """
 import logging
 import json
+from datetime import timedelta
 import voluptuous as vol
 
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pylinky==0.1.3']
+REQUIREMENTS = ['pylinky==0.1.5']
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'linky'
+
+TIME_BETWEEN_UPDATES = timedelta(days=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string
 })
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Configure the platform and add the Linky sensor."""
     username = config.get(CONF_USERNAME)
     password = config.get(CONF_PASSWORD)
 
     from pylinky.client import LinkyClient
     client = LinkyClient(username, password)
 
-    try:
-        client.fetch_data()
-    except BaseException as exp:
-        _LOGGER.error(exp)
-    finally:
-        client.close_session()
+    # get the last past day data
+    devices = [LinkySensor('Linky', client)]
+    add_devices(devices, True)
 
-    _LOGGER.info(json.dumps(client.get_data(), indent=2))
-
-    add_devices([LinkySensor('Linky', {})], True)
 
 class LinkySensor(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self, name, linky_data):
+    def __init__(self, name, client):
         self._name = name
-        self._linky_data = linky_data
+        self._client = client
         self._state = None
 
     @property
@@ -56,6 +56,7 @@ class LinkySensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
+        self.update()
         return self._state
 
     @property
@@ -63,6 +64,19 @@ class LinkySensor(Entity):
         """Return the unit of measurement."""
         return 'kWh'
 
+    @Throttle(TIME_BETWEEN_UPDATES)
     def update(self):
         """Fetch new state data for the sensor."""
-        self._state = 23
+        try:
+            self._client.fetch_data()
+        except BaseException as exp:
+            _LOGGER.error(exp)
+        finally:
+            self._client.close_session()
+
+        _LOGGER.debug(json.dumps(self._client.get_data(), indent=2))
+
+        if self._client.get_data():
+            self._state = self._client.get_data().get('daily')[-2].get('conso')
+        else:
+            self._state = 0
