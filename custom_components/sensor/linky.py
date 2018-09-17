039abc9e65d52a35fb7e2edb2a0cc5e2f2a317b5
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2018-09-17 08:49:12 +0200

    fix: add timeout

--- custom_components/sensor/linky.py
+++ custom_components/sensor/linky.py
@@ -10,36 +10,42 @@ from datetime import timedelta
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_TIMEOUT
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pylinky==0.1.5']
+REQUIREMENTS = ['pylinky==0.1.6']
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'linky'
-
 SCAN_INTERVAL = timedelta(minutes=10)
+DEFAULT_TIMEOUT = 10
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_PASSWORD): cv.string
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
 })
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     """Configure the platform and add the Linky sensor."""
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+    timeout = config[CONF_TIMEOUT]
 
-    from pylinky.client import LinkyClient
-    client = LinkyClient(username, password)
+    from pylinky.client import LinkyClient, PyLinkyError
+    client = LinkyClient(username, password, None, timeout)
 
-    # get the last past day data
+    try:
+        client.fetch_data()
+    except PyLinkyError as exp:
+        _LOGGER.error(exp)
+        return
+
     devices = [LinkySensor('Linky', client)]
-    add_devices(devices, True)
+    add_entities(devices, True)
 
 
 class LinkySensor(Entity):
@@ -69,16 +75,17 @@ class LinkySensor(Entity):
     @Throttle(SCAN_INTERVAL)
     def update(self):
         """Fetch new state data for the sensor."""
+        from pylinky.client import PyLinkyError
         try:
             self._client.fetch_data()
-        except BaseException as exp:
+        except PyLinkyError as exp:
             _LOGGER.error(exp)
-        finally:
-            self._client.close_session()
+            return
 
         _LOGGER.debug(json.dumps(self._client.get_data(), indent=2))
 
         if self._client.get_data():
+            # get the last past day data
             self._state = self._client.get_data()['daily'][-2]['conso']
         else:
-            self._state = 0
+            self._state = None
